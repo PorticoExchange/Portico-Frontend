3c79bcfc777cbbf4f0c5bb92c45e3d7c60f68700
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-11 12:28:34 -0300

    Update process_swap.js

--- data/process_swap.js
+++ data/process_swap.js
@@ -1,4 +1,8 @@
 const swap = require ('Portico-Swap');
 const bitcoin = require ('bitcoin-js');
 
-const bitcoi.network = bitcoin.network.testnet
+const bitcoin.network = bitcoin.network.testnet
+
+const swap {
+  id:
+  tx:
